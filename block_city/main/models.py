e98a3788a57861d514b20f7d90f57768456483e2
user <sergeypozhoga@mail.ru>
2023-06-26 00:18:24 +0300

    Initial commit

---
+++ block_city/main/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title=models.CharField(max_length=100)
+    description = models.TextField()
+    text = models.TextField()
+    image = models.FileField(upload_to='img/')
